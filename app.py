bb580f02749fbd3b506ff2add89d3b52550d1d86
TouyeAchille <touyejunior@gmail.com>
2025-08-01 14:17:54 +0000

    update app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
-
+import os 
 import pandas as pd
 import joblib
 from sklearn.preprocessing import StandardScaler
@@ -13,7 +13,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info("Scaling Payload: %s payload")
+    LOG.info("Scaling Payload: %s", "payload")
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
@@ -21,7 +21,7 @@ def scale(payload):
 @app.route("/")
 def home():
     html = "<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    return html
 
 # TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
@@ -33,16 +33,17 @@ def predict():
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
         # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
     except:
-        LOG.info("JSON payload: %s json_payload")
+        LOG.info("JSON payload: %s", str(e))
         return "Model not loaded"
 
     json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
+    LOG.info("JSON payload: %s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
+    LOG.info("inference payload DataFrame: %s", inference_payload)
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port= int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
